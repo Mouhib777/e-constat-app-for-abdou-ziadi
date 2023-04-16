675221af35232cb171a3c25fb4c369318b80deb1
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-16 21:15:27 +0000

    c

--- lib/screens/loginRegistre.dart
+++ lib/screens/loginRegistre.dart
@@ -1,4 +1,6 @@
 import 'package:e_constat/constant/constant.dart';
+import 'package:e_constat/screens/login.dart';
+import 'package:e_constat/screens/registreScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -29,11 +31,75 @@ class _loginRegistreState extends State<loginRegistre> {
         appBar: AppBar(
           leading: Column(),
           backgroundColor: primaryColor,
-          title: Text(
-            'E-Constat',
-            style: GoogleFonts.raleway(),
+          title: ZoomIn(
+            child: Text(
+              'E-Constat',
+              style: GoogleFonts.raleway(),
+            ),
           ),
           centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeIn(
+                delay: Duration(seconds: 2),
+                child: SizedBox(
+                  width: 170,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => registreScreen()));
+                    },
+                    child: Text(
+                      "S'inscrire",
+                      style: GoogleFonts.raleway(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide.none,
+                          borderRadius: BorderRadius.circular(14)),
+                      backgroundColor: primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              FadeIn(
+                delay: Duration(seconds: 2),
+                child: SizedBox(
+                  width: 170,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => loginScreen()));
+                    },
+                    child: Text(
+                      "Se connecter",
+                      style: GoogleFonts.raleway(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide.none,
+                          borderRadius: BorderRadius.circular(14)),
+                      backgroundColor: primaryColor,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/registreScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class registreScreen extends StatefulWidget {
+  const registreScreen({super.key});
+
+  @override
+  State<registreScreen> createState() => _registreScreenState();
+}
+
+class _registreScreenState extends State<registreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
