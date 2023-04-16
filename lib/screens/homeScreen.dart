60226e9cfd00bb52e15c1afc6c68b6d583cf6b78
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-16 20:39:48 +0000

    c

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,9 @@
+import 'package:e_constat/constant/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animate_do/animate_do.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -18,12 +21,93 @@ class _homeScreenState extends State<homeScreen> {
             image: AssetImage(
               "assets/images/road.jpg",
             ),
-            fit: BoxFit.fill),
+            fit: BoxFit.fill,
+            opacity: 210),
       ),
       child: Scaffold(
+        appBar: AppBar(
+          shadowColor: Colors.transparent,
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.transparent,
+          surfaceTintColor: Colors.transparent,
+        ),
         backgroundColor: Colors.transparent,
-        body: Column(
-          children: [],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              FlipInY(
+                child: Text(
+                  "Bienvenue",
+                  style: GoogleFonts.raleway(
+                      letterSpacing: 3,
+                      color: thirdColor,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 300,
+              ),
+              FadeInDown(
+                child: Text(
+                  "E- constat auto",
+                  style: GoogleFonts.raleway(
+                      letterSpacing: 4,
+                      color: secondaryColor,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              FadeInUp(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "l'application officielle des assureurs tunisiennes",
+                      style: GoogleFonts.raleway(
+                          letterSpacing: 1,
+                          color: thirdColor,
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "🇹🇳",
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 130,
+              ),
+              SizedBox(
+                width: 170,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Commencez",
+                    style: GoogleFonts.raleway(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide.none,
+                        borderRadius: BorderRadius.circular(20)),
+                    backgroundColor: primaryColor,
+                    //Color.fromARGB(255, 230, 144, 118),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
