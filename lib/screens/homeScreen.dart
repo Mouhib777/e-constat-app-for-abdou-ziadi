5559f37ddc741843a6f876b9f0cacc756934a4b3
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-16 20:12:53 +0000

    c

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -14,9 +14,17 @@ class _homeScreenState extends State<homeScreen> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/images/road.jpg"))),
+        image: DecorationImage(
+            image: AssetImage(
+              "assets/images/road.jpg",
+            ),
+            fit: BoxFit.fill),
+      ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
+        body: Column(
+          children: [],
+        ),
       ),
     );
   }
