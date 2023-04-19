aa1d67b812f5a89cd69ee1321cf56e87f936a293
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-19 04:39:48 +0000

    c

--- lib/screens/mapScreen.dart
+++ lib/screens/mapScreen.dart
@@ -1,8 +1,10 @@
+import 'package:e_constat/providers/locationProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 class mapScreen extends StatefulWidget {
   const mapScreen({super.key});
@@ -12,8 +14,15 @@ class mapScreen extends StatefulWidget {
 }
 
 class _mapScreenState extends State<mapScreen> {
+  late LatLng currentLocation1;
+  late GoogleMapController _mapController;
+  late String AdressLine;
+  bool _locating = false;
+  bool _loggedIn = false;
+
   @override
   Widget build(BuildContext context) {
+    final locationData = Provider.of<LocationProvider>(context);
     return Scaffold(
       body: Stack(
         children: [
