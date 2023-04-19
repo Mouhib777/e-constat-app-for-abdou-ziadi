fc7a9a1a044ae86eb2e34741e72b9703e7d4b3a8
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-19 03:58:31 +0000

    c

--- lib/screens/econstat.dart
+++ lib/screens/econstat.dart
@@ -76,22 +76,52 @@ class _EconstatState extends State<Econstat> {
                   currentStep -= 1;
                 });
         },
-        controlsBuilder:(context, onStepContinue) {
+        controlsBuilder: (BuildContext context, ControlsDetails) {
+          final lastStep = currentStep == getSteps().length - 1;
           return Container(
             margin: EdgeInsets.only(top: 50),
-            child:  Row(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(child: ElevatedButton(
-                  onPressed: onStepContinue ,
-                   child: Text('suivant')
-                   )
-                   )
+                SizedBox(
+                  width: 100,
+                  child: Expanded(
+                      child: ElevatedButton(
+                    onPressed: ControlsDetails.onStepContinue,
+                    child: Text(
+                      lastStep ? 'Confimer' : 'Suivant',
+                      style: GoogleFonts.raleway(),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide.none,
+                            borderRadius: BorderRadius.circular(15))),
+                  )),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                if (currentStep != 0)
+                  SizedBox(
+                    width: 100,
+                    child: Expanded(
+                        child: ElevatedButton(
+                      onPressed: ControlsDetails.onStepCancel,
+                      child: Text(
+                        'Précedent',
+                        style: GoogleFonts.raleway(),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide.none,
+                              borderRadius: BorderRadius.circular(15))),
+                    )),
+                  )
               ],
             ),
-          )
+          );
         },
-        
-        ,
+
         // onStepTapped: (step) {
         //   setState(() {
         //     currentStep = step;
