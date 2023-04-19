3dc12d269ea841a74603687cc69c845685af1aae
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-19 04:21:32 +0000

    c

---
+++ lib/screens/mapScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class mapScreen extends StatefulWidget {
+  const mapScreen({super.key});
+
+  @override
+  State<mapScreen> createState() => _mapScreenState();
+}
+
+class _mapScreenState extends State<mapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "ou l'accident",
+          style: GoogleFonts.raleway(),
+        ),
+      ),
+    );
+  }
+}
