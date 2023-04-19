4fd82e689a2c9655af34368a16de58de962a67fc
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-19 04:49:35 +0000

    c

--- lib/screens/mapScreen.dart
+++ lib/screens/mapScreen.dart
@@ -2,6 +2,7 @@ import 'package:e_constat/providers/locationProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
@@ -14,11 +15,26 @@ class mapScreen extends StatefulWidget {
 }
 
 class _mapScreenState extends State<mapScreen> {
-  LatLng? currentLocation1;
+  late LatLng currentLocation1;
   late GoogleMapController _mapController;
   late String AdressLine;
   bool _locating = false;
   bool _loggedIn = false;
+  Future initState() async {
+    final locationData = Provider.of<LocationProvider>(context, listen: false);
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+    await locationData.getCurrentPosition();
+    // if (locationData.permissionAllowed == true) {
+    //   Navigator.push(
+    //       context, MaterialPageRoute(builder: (context) => MainScreen()));
+    //   //EasyLoading.showInfo("Add Your Pick UP Address ");
+    // } else {
+    //   print('permission NOT ALLOWED');
+    // }
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +44,7 @@ class _mapScreenState extends State<mapScreen> {
         children: [
           GoogleMap(
             initialCameraPosition: CameraPosition(
-              target: currentLocation1!,
+              target: currentLocation1,
               zoom: 14.4746,
             ),
           )
