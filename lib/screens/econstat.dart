2bd5f32e3be12b442a406f32013c91c0bec700b6
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-19 04:15:31 +0000

    c

--- lib/screens/econstat.dart
+++ lib/screens/econstat.dart
@@ -43,6 +43,7 @@ class _EconstatState extends State<Econstat> {
       ];
 
   int currentStep = 0;
+  bool isCompleted = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,80 +55,79 @@ class _EconstatState extends State<Econstat> {
         ),
         centerTitle: true,
       ),
-      body: Stepper(
-        elevation: 4,
-        type: StepperType.horizontal,
-        steps: getSteps(),
-        currentStep: currentStep,
-        onStepContinue: () {
-          final lastStep = currentStep == getSteps().length - 1;
-          if (lastStep) {
-            print('okk');
-          } else {
-            setState(() {
-              currentStep += 1;
-            });
-          }
-        },
-        onStepCancel: () {
-          currentStep == 0
-              ? null
-              : setState(() {
-                  currentStep -= 1;
-                });
-        },
-        controlsBuilder: (BuildContext context, ControlsDetails) {
-          final lastStep = currentStep == getSteps().length - 1;
-          return Container(
-            margin: EdgeInsets.only(top: 50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 100,
-                  child: Expanded(
-                      child: ElevatedButton(
-                    onPressed: ControlsDetails.onStepContinue,
-                    child: Text(
-                      lastStep ? 'Confirmer' : 'Suivant',
-                      style: GoogleFonts.raleway(),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide.none,
-                            borderRadius: BorderRadius.circular(15))),
-                  )),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                if (currentStep != 0)
-                  SizedBox(
-                    width: 100,
-                    child: Expanded(
-                        child: ElevatedButton(
-                      onPressed: ControlsDetails.onStepCancel,
-                      child: Text(
-                        'Précedent',
-                        style: GoogleFonts.raleway(),
+      body: isCompleted
+          ? buildCompleted()
+          : Stepper(
+              elevation: 4,
+              type: StepperType.horizontal,
+              steps: getSteps(),
+              currentStep: currentStep,
+              onStepContinue: () {
+                final lastStep = currentStep == getSteps().length - 1;
+                if (lastStep) {
+                  setState(() {
+                    isCompleted = true;
+                  });
+                  print('okk');
+                } else {
+                  setState(() {
+                    currentStep += 1;
+                  });
+                }
+              },
+              onStepCancel: () {
+                currentStep == 0
+                    ? null
+                    : setState(() {
+                        currentStep -= 1;
+                      });
+              },
+              controlsBuilder: (BuildContext context, ControlsDetails) {
+                final lastStep = currentStep == getSteps().length - 1;
+                return Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Expanded(
+                            child: ElevatedButton(
+                          onPressed: ControlsDetails.onStepContinue,
+                          child: Text(
+                            lastStep ? 'Confirmer' : 'Suivant',
+                            style: GoogleFonts.raleway(),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(15))),
+                        )),
                       ),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide.none,
-                              borderRadius: BorderRadius.circular(15))),
-                    )),
-                  )
-              ],
+                      SizedBox(
+                        width: 15,
+                      ),
+                      if (currentStep != 0)
+                        SizedBox(
+                          width: 100,
+                          child: Expanded(
+                              child: ElevatedButton(
+                            onPressed: ControlsDetails.onStepCancel,
+                            child: Text(
+                              'Précedent',
+                              style: GoogleFonts.raleway(),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    side: BorderSide.none,
+                                    borderRadius: BorderRadius.circular(15))),
+                          )),
+                        )
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-
-        // onStepTapped: (step) {
-        //   setState(() {
-        //     currentStep = step;
-        //   });
-        // },
-      ),
     );
   }
 }
