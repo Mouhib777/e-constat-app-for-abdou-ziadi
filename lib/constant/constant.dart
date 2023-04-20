294c435e555ce0ceea172b2d43368b1aaf0ad531
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-20 00:21:10 +0000

    c

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xff282634);
 const secondaryColor = Color(0xffff4057);
 const thirdColor = Color(0xffdcdcdc);
+const GoogleMap_Api_Key = "AIzaSyCIfbsxH7UomLuyFdSdF4TZ2r57cVkP0fo";
