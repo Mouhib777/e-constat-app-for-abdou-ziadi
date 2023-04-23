be977857529ab1e0c7e6ddb90460037d159268d2
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-23 08:19:21 +0000

    c

--- lib/screens/paint.dart
+++ lib/screens/paint.dart
@@ -38,80 +38,86 @@ class _paintScreenState extends State<paintScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "croquis de l'accident",
-          style: GoogleFonts.raleway(
-              letterSpacing: 3, fontWeight: FontWeight.w500),
+        appBar: AppBar(
+          title: Text(
+            "croquis de l'accident",
+            style: GoogleFonts.raleway(
+                letterSpacing: 3, fontWeight: FontWeight.w500),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.9,
-              height: MediaQuery.of(context).size.height * 0.75,
-              child: GestureDetector(
-                onPanDown: (details) => _startStroke(
-                  details.localPosition.dx,
-                  details.localPosition.dy,
+        body: Center(
+          child: croquis_capture != null
+              ? Container()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.height * 0.75,
+                        child: GestureDetector(
+                          onPanDown: (details) => _startStroke(
+                            details.localPosition.dx,
+                            details.localPosition.dy,
+                          ),
+                          onPanUpdate: (details) => _moveStroke(
+                              details.localPosition.dx,
+                              details.localPosition.dy),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height,
+                            color: thirdColor,
+                            child: CustomPaint(
+                              painter: DrawingPainter(_strokes),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(PageRouteBuilder(
+                                  transitionDuration: Duration.zero,
+                                  pageBuilder: (context, animation,
+                                          secondaryAnimation) =>
+                                      paintScreen()));
+                            },
+                            child: Text(
+                              "effacer tout",
+                              style: GoogleFonts.raleway(letterSpacing: 3),
+                            )),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        ElevatedButton(
+                            onPressed: () async {
+                              String? croquis =
+                                  await NativeScreenshot.takeScreenshot();
+                              if (croquis != null) {
+                                setState(() {
+                                  croquis_capture = File(croquis);
+                                });
+                              } else {
+                                print("erreur");
+                                EasyLoading.showError('Erreur');
+                              }
+                            },
+                            child: Text(
+                              "Envoyer le croquis",
+                              style: GoogleFonts.raleway(letterSpacing: 2),
+                            )),
+                      ],
+                    ),
+                  ],
                 ),
-                onPanUpdate: (details) => _moveStroke(
-                    details.localPosition.dx, details.localPosition.dy),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  color: thirdColor,
-                  child: CustomPaint(
-                    painter: DrawingPainter(_strokes),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(PageRouteBuilder(
-                        transitionDuration: Duration.zero,
-                        pageBuilder: (context, animation, secondaryAnimation) =>
-                            paintScreen()));
-                  },
-                  child: Text(
-                    "effacer tout",
-                    style: GoogleFonts.raleway(letterSpacing: 3),
-                  )),
-              SizedBox(
-                width: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    String? croquis = await NativeScreenshot.takeScreenshot();
-                    if (croquis != null) {
-                      setState(() {
-                        croquis_capture = File(croquis);
-                      });
-                    } else {
-                      print("erreur");
-                      EasyLoading.showError('Erreur');
-                    }
-                  },
-                  child: Text(
-                    "Envoyer le croquis",
-                    style: GoogleFonts.raleway(letterSpacing: 2),
-                  )),
-            ],
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
 
