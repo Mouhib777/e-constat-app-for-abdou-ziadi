4bb8a67951949e6aa6676cc9dfbcb208dff4c7b6
Mouhib777 <mouhibsahbani@gmail.com>
2023-04-23 07:51:06 +0000

    c

--- lib/screens/paint.dart
+++ lib/screens/paint.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:native_screenshot/native_screenshot.dart';
 
 class paintScreen extends StatefulWidget {
   const paintScreen({super.key});
